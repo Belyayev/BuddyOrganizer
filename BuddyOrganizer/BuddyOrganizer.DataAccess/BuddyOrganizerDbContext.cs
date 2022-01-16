b83980f9fe91b979e4a8a33fba5bebdcdfaf282e
Mike <4xgood@gmail.com>
2022-01-16 06:16:42 -0700

    5-Create a DbContext Subclass

---
+++ BuddyOrganizer/BuddyOrganizer.DataAccess/BuddyOrganizerDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BuddyOrganizer.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace BuddyOrganizer.DataAccess
+{
+    public class BuddyOrganizerDbContext : DbContext
+    {
+        public BuddyOrganizerDbContext():base("BuddyOrganizerDb")
+        {
+
+        }
+        public DbSet<Buddy> Buddies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
