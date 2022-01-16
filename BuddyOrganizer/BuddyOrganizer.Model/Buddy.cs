5af0bfcc2a4b9d224322d87a728065e646b1b857
Mike <4xgood@gmail.com>
2022-01-16 06:38:39 -0700

    5-Add Constraints

--- BuddyOrganizer/BuddyOrganizer.Model/Buddy.cs
+++ BuddyOrganizer/BuddyOrganizer.Model/Buddy.cs
@@ -1,10 +1,19 @@
-﻿namespace BuddyOrganizer.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuddyOrganizer.Model
 {
     public class Buddy
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
+
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
