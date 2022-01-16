0b8ae3ab146eab34723f726986d4c5ca3364a1a7
Mike <4xgood@gmail.com>
2022-01-16 06:29:21 -0700

    5-Add a code first migration

---
+++ BuddyOrganizer/BuddyOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,27 @@
+﻿namespace BuddyOrganizer.DataAccess.Migrations
+{
+    using BuddyOrganizer.Model;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BuddyOrganizer.DataAccess.BuddyOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BuddyOrganizer.DataAccess.BuddyOrganizerDbContext context)
+        {
+            context.Buddies.AddOrUpdate(
+                f => f.FirstName,
+                new Buddy { FirstName = "Mike", LastName = "White" },
+                new Buddy { FirstName = "John", LastName = "Doe" },
+                new Buddy { FirstName = "Kate", LastName = "White" },
+                new Buddy { FirstName = "Allan", LastName = "Smith" }
+                );
+        }
+    }
+}
